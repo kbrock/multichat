367cbb0a7847ec94d84574d34c7aea8e064ccffd
Keenan Brock <keenan@thebrocks.net>
2014-11-23 23:14:06 -0500

    event machine pure chat
    poundchat -clients 140 -time 3s
    
    140  clients  3s
    140  clients exiting 0.6s
    
    8260  msgs sent
    1156400  packets received
    1156400  msgs received (match)
    
    (with 150, worst numbers)
    973735  packets received

---
+++ ruby-em/pure_chat.rb
@@ -0,0 +1,47 @@
+require 'em-websocket'
+
+$counter = 1
+
+class Hub
+  attr_accessor :connections
+
+  def initialize
+    @connections = []
+    @counter = 0
+  end
+
+  def broadcast(msg)
+    connections.each { |s| s[:socket].send msg }
+  end
+
+  def register(ws)
+    connections.push(id: next_id, socket: ws)
+  end
+
+  # returns client (hash)
+  def unregister(ws)
+    index = connections.index { |i| i[:socket] == ws }
+    connections.delete_at(index)
+  end
+
+  def next_id
+    @counter+=1
+  end
+end
+
+hub = Hub.new
+
+puts "listening on 8080"
+EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
+  ws.onopen do
+    hub.register(ws)
+  end
+
+  ws.onclose do
+    hub.unregister(ws)
+  end
+
+  ws.onmessage do |msg|
+    hub.broadcast(msg)
+  end
+end
